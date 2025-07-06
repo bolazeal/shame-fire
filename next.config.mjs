a0accdb2bd41f9ff4c8e5c93410d2879e22aa4ed
Abolaji Rasaq <bolarzeal@gmail.com>
2025-07-06 16:39:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  // These settings are helpful for debugging build issues.
-  // They prevent the build from failing on TypeScript or ESLint errors.
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
   },
 };
 

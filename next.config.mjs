a8eb2be9104f09bdb11636190693000ed09473d1
Abolaji Rasaq <bolarzeal@gmail.com>
2025-09-13 11:26:38 +0000

    next

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,30 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'storage.googleapis.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-            }
-        ],
-    },
-    typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
       },
-      eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
-
-    

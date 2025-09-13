c3a7e9ee0d8f716700d5129e8821f67a09266cd5
Abolaji Rasaq <bolarzeal@gmail.com>
2025-09-13 09:59:38 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: 'standalone',
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-            },
-            {
-                protocol: 'https',
-                hostname: 'storage.googleapis.com',
-            }
-        ],
-    },
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      }
+    ],
+  },
 };
 
 export default nextConfig;

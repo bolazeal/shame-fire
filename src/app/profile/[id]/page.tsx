ef8abfd3c4b01abf22bad180b9b22c741846bc28
Abolaji Rasaq <bolarzeal@gmail.com>
2025-09-13 11:33:43 +0000

    the site is slow to load when i click on any item

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,4 @@
 
-'use client';
-
 import { PostCard } from '@/components/post-card';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';

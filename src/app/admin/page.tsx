32eced7534d3d9c61b332e163f35a566c52f9a41
Abolaji Rasaq <bolarzeal@gmail.com>
2025-09-23 17:29:51 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: AlertDi

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -80,6 +80,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { UserAvatar } from '@/components/user-avatar';
 import {
